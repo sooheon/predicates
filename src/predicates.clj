6b5d8847681c8a065b0a81f96babfb2bf9ea29ef
sooheon <tngjs0@gmail.com>
2014-12-08 15:09:35 +0900

    nearly finished

--- src/predicates.clj
+++ src/predicates.clj
@@ -1,41 +1,88 @@
 (ns predicates)
 
+(defn apply-1 [f x]
+  (f x))
+
 (defn sum-f [f g x]
-  :-)
+  (+ (f x) (g x)))
+
+(defn greater-than [n]
+  (fn [k] (> k n)))
 
 (defn less-than [n]
-  :-)
+  (fn [k] (< k n)))
 
 (defn equal-to [n]
-  :-)
+  (fn [k] (== k n)))
+
+(defn key->predicate [a-key]
+  (fn [a-map] (contains? a-map a-key)))
 
 (defn set->predicate [a-set]
-  :-)
+  (fn [x] (contains? a-set x)))
 
 (defn pred-and [pred1 pred2]
-  :-)
+  (fn [x] (and (pred1 x) (pred2 x))))
 
 (defn pred-or [pred1 pred2]
-  :-)
+  )
 
 (defn whitespace? [character]
   (Character/isWhitespace character))
 
 (defn blank? [string]
-  :-)
+  (every? whitespace? string))
 
 (defn has-award? [book award]
-  :-)
+  (not (nil? (award (:awards book)))))
 
 (defn HAS-ALL-THE-AWARDS? [book awards]
-  :-)
+  (every? (set->predicate (:awards book)) awards))
 
 (defn my-some [pred a-seq]
-  :-)
+  (first (filter #(not (false? %)) (map pred a-seq))))
 
 (defn my-every? [pred a-seq]
-  :-)
+  )
 
 (defn prime? [n]
-  :-)
-;^^
+  )
+;;^^
+
+(def graphic-novels [{:name "Yotsuba 1" :series "Yotsuba"}
+                     {:name "Yotsuba 5" :series "Yotsuba"}
+                     {:name "Persepolis"}
+                     {:name "That Yellow Bastard" :series "Sin City"}
+                     {:name "The Hard Goodbye" :series "Sin City"}
+                     {:name "Maus"}
+                     {:name "Solanin"}
+                     {:name "Monster 3" :series "Monster"}])
+
+(defn non-negatives [a-seq]
+  (filter (complement neg?) a-seq))
+
+(defn average
+  [numbers]
+  (/ (apply + numbers) (count numbers)))
+
+(def china {:name "China Miéville", :birth-year 1972})
+(def octavia {:name "Octavia E. Butler"
+              :birth-year 1947
+              :death-year 2006})
+(def kay {:name "Guy Gavriel Kay" :birth-year 1954})
+(def dick {:name "Philip K. Dick", :birth-year 1928, :death-year 1982})
+(def zelazny {:name "Roger Zelazny", :birth-year 1937, :death-year 1995})
+
+(def authors #{china, octavia, kay, dick, zelazny})
+
+(def cities {:title "The City and the City" :authors #{china}
+             :awards #{:locus, :world-fantasy, :hugo}})
+(def wild-seed {:title "Wild Seed", :authors #{octavia}})
+(def lord-of-light {:title "Lord of Light", :authors #{zelazny}
+                    :awards #{:hugo}})
+(def deus-irae {:title "Deus Irae", :authors #{dick, zelazny}})
+(def ysabel {:title "Ysabel", :authors #{kay}, :awards #{:world-fantasy}})
+(def scanner-darkly {:title "A Scanner Darkly" :authors #{dick}})
+
+(def books #{cities, wild-seed, lord-of-light,
+             deus-irae, ysabel, scanner-darkly})
